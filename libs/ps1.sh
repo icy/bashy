7cba2f5e65850ee5f8ce976c56febfaede70f670
Ky-Anh Huynh <kyanh@theslinux.org>
2014-11-26 22:43:16 +0700

    ps1: initially add

---
+++ libs/ps1.sh
@@ -0,0 +1,6 @@
+# Purpose: Provide Bash PS1
+# Author : Anh K. Huynh
+# Date   : 2010 (v1), 2014 (v2)
+# Usage  : source $0
+
+export PS1='\[\e[0;32m\]:: \[\e[0;37m\]You are \[\e[0;31m\]\u\[\e[0;37m\]@\[\e[0;31m\]\h\[\e[0;37m\] \[\e[0;37m\]\w\n\[\e[0;32m\]\$\[\e[0m\] '
