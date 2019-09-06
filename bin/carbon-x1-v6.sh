af36d540678144b62de28db9ac7b989477ae0f34
Ky-Anh Huynh <kyanh@viettug.org>
2019-09-06 16:19:27 +0200

    Add bin/*

---
+++ bin/carbon-x1-v6.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Purpose : Booting up ThinkPad Carbon X1-v6
+# Author  : Mr. Internet
+# Date    : 2019-09-02
+
+set -x
+
+# Disabling the memory card reader
+echo "2-3" | sudo tee /sys/bus/usb/drivers/usb/unbind || true
